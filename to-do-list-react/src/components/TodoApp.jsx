b1d7d3a24c640f4fc12f0754f7c046f9ae90c6ba
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-02 12:34:08 -0400

    Add Header and TodosLogic components to TodoApp

--- to-do-list-react/src/components/TodoApp.jsx
+++ to-do-list-react/src/components/TodoApp.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '@/components/Header';
 
 const TodoApp = () => {
   return (
     <>
-      <h1>Hello world!</h1>
-      <p>I am in a React Component!</p>
+      <Header />
+      <TodosLogic />
     </>
   );
 };
