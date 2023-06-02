8c750fbc12e75373e635fa537c646064bf9ab9fb
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-02 13:28:42 -0400

    Add delTodo function to delete an item when his delete button is pressed

--- to-do-list-react/src/components/TodoItem.jsx
+++ to-do-list-react/src/components/TodoItem.jsx
@@ -1,4 +1,4 @@
-const TodoItem = ({ itemProp, handleChange }) => {
+const TodoItem = ({ itemProp, handleChange, delTodo }) => {
   return (
     <li>
       <input
@@ -6,7 +6,7 @@ const TodoItem = ({ itemProp, handleChange }) => {
         checked={itemProp.completed}
         onChange={() => handleChange(itemProp.id)}
       />
-      <button>Delete</button>
+      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
       {itemProp.title}
     </li>
   );
